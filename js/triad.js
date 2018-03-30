4ee84d67a3b25ef29a4faab59a49fb9fba738665
ENOSISBD\sourov <sourov0805045@gmail.com>
2018-03-30 18:55:54 +0600

    Added triad geometry.

---
+++ js/triad.js
@@ -0,0 +1,296 @@
+/**
+ * Created by sourov on 30-Mar-18.
+ */
+
+
+function Triad() {
+
+
+    let self = this;
+
+    let positionPerAxis = 14;
+    let totalAxisCount = 3;
+    let componentPerPosition = 3;
+    let componentPerMeshid = 1;
+    let trianglePerAxis = 20;
+    let componentPerTriangle = 3;
+
+    self.triadObject = new THREE.Object3D();
+
+    function createTriad() {
+        let geometry = createTriadGeometry();
+        let material = createTriadMaterial();
+
+        let triadMesh = new THREE.Mesh(geometry, material);
+        triadMesh.name = 'Triad';
+        return triadMesh;
+    }
+
+    function createTriadMaterial() {
+
+    }
+
+    function createTriadGeometry() {
+        let geometry = new THREE.BufferGeometry();
+
+        let positionArray = getTriadPositionArray();
+        let colorArray = getTriadColorArray();
+        let indexArray = getTriadIndexArray();
+
+        geometry.addAttribute('position', new THREE.BufferAttribute(positionArray, 3));
+        geometry.addAttribute('color', new THREE.BufferAttribute(colorArray, 3));
+        geometry.addIndex(indexArray);
+
+        return geometry;
+    }
+
+    function getPositionArraySize() {
+        return positionPerAxis * totalAxisCount * componentPerPosition;
+    }
+
+    function getTriadPositionArray() {
+
+        let positionArray = new Float32Array(getPositionArraySize());
+
+        fillXAxisPositions(positionArray);
+        fillYAxisPositions(positionArray);
+        fillZAxisPositions(positionArray);
+
+
+        return positionArray;
+
+    }
+
+    function fillXAxisPositions(positionArray) {
+        fillAxisPositions(positionArray, 0);
+    }
+
+    function fillYAxisPositions(positionArray) {
+        let rotationMatrix = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3().set(0, 0, 1), THREE.Math.degToRad(90));
+        fillAxisPositions(positionArray, 0, positionPerAxis * componentPerPosition, rotationMatrix);
+    }
+
+    function fillZAxisPositions(positionArray) {
+        let rotationMatrix = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3().set(0, 1, 0), THREE.Math.degToRad(90));
+        fillAxisPositions(positionArray, 0, 2 * positionPerAxis * componentPerPosition, rotationMatrix);
+    }
+
+    function fillAxisPositions(positionArray, start, transformationMatrix) {
+        let allPositions = [];
+
+        let templatePositions = getPositionArrayTemplate();
+        let mirroredPositions = getMirrorAlongXAxis(templatePositions);
+
+        templatePositions.forEach(function(position) {
+            allPositions.push(position);
+        });
+
+        mirroredPositions.forEach(function(position) {
+            allPositions.push(position);
+        });
+
+        for (let i = start, count = 0; count < allPositions.length; i += 3, count++ ) {
+
+            let position = allPositions[count];
+
+            if (transformationMatrix)
+                position.applyMatrix(transformationMatrix);
+
+            positionArray[i + 0] = position.x;
+            positionArray[i + 1] = position.y;
+            positionArray[i + 2] = position.z;
+        }
+    }
+
+    function getPositionArrayTemplate() {
+        let positionArrayTemplate = [];
+
+        // tail
+        positionArrayTemplate.push(new THREE.Vector3(0, 1, 0.5));
+        positionArrayTemplate.push(new THREE.Vector3(10, 1, 0.5));
+        positionArrayTemplate.push(new THREE.Vector3(10, -1, 0.5));
+        positionArrayTemplate.push(new THREE.Vector3(0, -1, 0.5));
+
+        // head
+        positionArrayTemplate.push(new THREE.Vector3(10, 1.5, 0.8));
+        positionArrayTemplate.push(new THREE.Vector3(15, 0, 0.8));
+        positionArrayTemplate.push(new THREE.Vector3(10, -1.5, 0.8));
+
+        return positionArrayTemplate;
+    }
+
+    function getMirrorAlongXAxis(positionArray) {
+        let mirroredPositionArray = [];
+
+        positionArray.forEach(function(position) {
+            mirroredPositionArray.push(new THREE.Vector3().set(position.x, position.y, position.z * -1));
+        });
+
+        return mirroredPositionArray;
+    }
+
+    function getTriadColorArray() {
+        let colorArray = new Float32Array(getPositionArraySize());
+
+        fillXAxisColors(colorArray);
+        fillYAxisColors(colorArray);
+        fillZAxisColors(colorArray);
+
+        return colorArray;
+    }
+
+    function fillXAxisColors(colorArray) {
+        // Red for X axis
+        for (let i = 0; i < positionPerAxis * componentPerPosition; i += 3) {
+            colorArray[i + 0] = 1.0;
+            colorArray[i + 1] = 0.0;
+            colorArray[i + 2] = 0.0;
+        }
+    }
+
+    function fillYAxisColors(colorArray) {
+        // Blue for Y axis
+        for (let i = positionPerAxis * componentPerPosition; i < 2 * positionPerAxis * componentPerPosition; i += 3) {
+            colorArray[i + 0] = 0.0;
+            colorArray[i + 1] = 0.0;
+            colorArray[i + 2] = 1.0;
+        }
+    }
+
+    function fillZAxisColors(colorArray) {
+        // Red for Z axis
+        for (let i = 2 * positionPerAxis * componentPerPosition; i < 3 * positionPerAxis * componentPerPosition; i += 3) {
+            colorArray[i + 0] = 0.0;
+            colorArray[i + 1] = 1.0;
+            colorArray[i + 2] = 0.0;
+        }
+    }
+
+    function getTriadIndexArray() {
+        let indexArray = new Uint16Array(getIndexArraySize());
+
+        fillXAxisIndexArray(indexArray);
+        fillYAxisIndexArray(indexArray);
+        fillZAxisIndexArray(indexArray);
+
+        return indexArray;
+    }
+
+    function getIndexArraySize() {
+        return totalAxisCount * trianglePerAxis * componentPerTriangle;
+    }
+
+    function fillXAxisIndexArray(indexArray) {
+        let indexArrayTemplate = getIndexArrayTemplate();
+
+        fillIndexArrayFromTemplate(indexArray, indexArrayTemplate, 0, trianglePerAxis * componentPerTriangle);
+    }
+
+    function fillYAxisIndexArray(indexArray) {
+        let indexArrayTemplate = getIndexArrayTemplate();
+
+        fillIndexArrayFromTemplate(indexArray, indexArrayTemplate, trianglePerAxis * componentPerTriangle, 2 * trianglePerAxis * componentPerTriangle);
+    }
+
+    function fillZAxisIndexArray(indexArray) {
+        let indexArrayTemplate = getIndexArrayTemplate();
+
+        fillIndexArrayFromTemplate(indexArray, indexArrayTemplate, 2 * trianglePerAxis * componentPerTriangle, 3 * trianglePerAxis * componentPerTriangle);
+    }
+
+    function fillIndexArrayFromTemplate(indexArray, indexArrayTemplate, start, end) {
+        for (let i = start; i < end; i++)
+            indexArray[i] = indexArrayTemplate[i - start];
+    }
+
+    function getIndexArrayTemplate() {
+        let indexArrayTemplate = [];
+
+        indexArrayTemplate.push(0);
+        indexArrayTemplate.push(3);
+        indexArrayTemplate.push(1);
+
+        indexArrayTemplate.push(1);
+        indexArrayTemplate.push(3);
+        indexArrayTemplate.push(2);
+
+        indexArrayTemplate.push(4);
+        indexArrayTemplate.push(6);
+        indexArrayTemplate.push(5);
+
+        indexArrayTemplate.push(7);
+        indexArrayTemplate.push(8);
+        indexArrayTemplate.push(10);
+
+        indexArrayTemplate.push(8);
+        indexArrayTemplate.push(10);
+        indexArrayTemplate.push(9);
+
+        indexArrayTemplate.push(0);
+        indexArrayTemplate.push(7);
+        indexArrayTemplate.push(3);
+
+        indexArrayTemplate.push(7);
+        indexArrayTemplate.push(10);
+        indexArrayTemplate.push(3);
+
+        indexArrayTemplate.push(1);
+        indexArrayTemplate.push(2);
+        indexArrayTemplate.push(8);
+
+        indexArrayTemplate.push(8);
+        indexArrayTemplate.push(2);
+        indexArrayTemplate.push(9);
+
+        indexArrayTemplate.push(0);
+        indexArrayTemplate.push(1);
+        indexArrayTemplate.push(8);
+
+        indexArrayTemplate.push(0);
+        indexArrayTemplate.push(8);
+        indexArrayTemplate.push(7);
+
+        indexArrayTemplate.push(3);
+        indexArrayTemplate.push(9);
+        indexArrayTemplate.push(10);
+
+        indexArrayTemplate.push(3);
+        indexArrayTemplate.push(2);
+        indexArrayTemplate.push(9);
+
+        indexArrayTemplate.push(4);
+        indexArrayTemplate.push(13);
+        indexArrayTemplate.push(11);
+
+        indexArrayTemplate.push(4);
+        indexArrayTemplate.push(6);
+        indexArrayTemplate.push(13);
+
+        indexArrayTemplate.push(4);
+        indexArrayTemplate.push(5);
+        indexArrayTemplate.push(12);
+
+        indexArrayTemplate.push(4);
+        indexArrayTemplate.push(12);
+        indexArrayTemplate.push(11);
+
+        indexArrayTemplate.push(5);
+        indexArrayTemplate.push(6);
+        indexArrayTemplate.push(13);
+
+        indexArrayTemplate.push(5);
+        indexArrayTemplate.push(13);
+        indexArrayTemplate.push(12);
+
+        indexArrayTemplate.push(11);
+        indexArrayTemplate.push(12);
+        indexArrayTemplate.push(13);
+
+        return indexArrayTemplate;
+    }
+
+}
+
+Triad.prototype = {
+
+};
